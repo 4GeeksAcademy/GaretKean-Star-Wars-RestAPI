9bba188146035a48d662fc68893d449381906865
GaretKean <167364720+GaretKean@users.noreply.github.com>
2024-09-06 02:06:54 +0000

    finished

--- src/app.py
+++ src/app.py
@@ -138,6 +138,11 @@ def delete_planet(planet_id):
 
 
 
+# <<<<<<<<<<<<-------------------------^^^^^^^^^^^^^^^^^^^^^^^PRACTICE MORE ROUTES^^^^^^^^^^^^^^^^^^^^^^^------------------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+
+
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
